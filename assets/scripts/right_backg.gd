fe34e295b8b84c67a8e166b8ade66836c3bb7b2c
Novais <floresnovais@gmail.com>
2024-07-05 19:51:18 +0100

    melhoria da função busStart
    alteração somente na cena RightBackg

--- assets/scripts/right_backg.gd
+++ assets/scripts/right_backg.gd
@@ -1,36 +1,58 @@
 extends Node2D
 
-@onready var bus_low = $BusLow
-@onready var bus_low_2 = $BusLow2
-@onready var bus_mid = $BusMid
-@onready var bus_full = $BusFull
+@onready var bus = $RightBackg/Bus
+@onready var right_backg = $RightBackg
+@onready var timer = $RightBackg/Timer
+
+# variaveis:
 
 var bus_delay
 var busactive = 0
+var elapse = 0
+
+#constantes:
+
+# para as posições 0 1 2 3
+const scales = [Vector2(0.018,0.021), Vector2(0.029,0.036),Vector2(0.077,0.09),Vector2(0.206,0.23)]
+const positions = [Vector2(50,-40), Vector2(77,-27.4),Vector2(7,-10.96),Vector2(-126.52,2.705)]
+
+const frequency = 5
+
+
 
 func busStart():
-	bus_delay = randi_range (5,20)
-	print(bus_delay)
+	print("COMEÇOU")
+	elapse = 0
 	busactive = 1
-	bus_low.visible = true
-	await get_tree().create_timer(bus_delay).timeout
-	bus_low.visible = false
-	bus_low_2.visible = true
-	await get_tree().create_timer(bus_delay).timeout
-	bus_low_2.visible = false
-	bus_mid.visible = true
-	await get_tree().create_timer(bus_delay).timeout
-	bus_mid.visible = false
-	bus_full.visible = true
-	await get_tree().create_timer(bus_delay).timeout
-	bus_full.visible = false
-	busactive = 0
-
-# Called when the node enters the scene tree for the first time.
+	var i = 0
+	var fst = 0
+	bus_delay = randi_range(10,20)
+	bus.flip_h = false
+	while(i != 4):
+		if(fst != 0):
+			bus.flip_h = true
+		bus.position = positions[i]
+		bus.scale = scales[i]
+		bus.visible = true
+		print(bus_delay)
+		await get_tree().create_timer(bus_delay).timeout # delay do bus 
+		bus_delay *= 0.60
+		i += 1
+	bus.visible = false
+	busactive = 0	
+	
 func _ready():
-	busStart()
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if(not busactive):
+		elapse += delta
+	if(elapse > frequency):
+		if(randi() % 2):
+			busStart()
+		else:
+			print("nao começou")
+			elapse = 0
+
